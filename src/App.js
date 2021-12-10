94938dc66f020c76521db006a4ff65345c5e174c
Shreyash Kolhe <90944900+shreyash510@users.noreply.github.com>
2021-12-10 13:53:43 +0530

    Cardview app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './Style/cardView.css';
+import CardView from './CardView';
+
+function App() {
+  return (
+    <>
+        <CardView/>
+    </>
+  );
+}
+
+export default App;
